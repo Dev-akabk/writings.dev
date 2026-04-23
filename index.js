66271a34ddd9dd3a570807434aec88c5479a16d8
Quangle <quang221208642@gmail.com>
2026-04-23 23:03:05 +0700

    Add .js, change html format

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const prevBtn = document.getElementById("prevBtn");
+
 const nextBtn = document.getElementById("nextBtn");
+
 const pageItems = document.querySelectorAll(".page-item");
 
 function updateActivePage(newIndex) {
   document.querySelector(".page-item.active").classList.remove("active");
+
   pageItems[newIndex].classList.add("active");
 }
+
+pageItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    updateActivePage(index);
+  });
+});
+
+nextBtn.addEventListener("click", () => {
+  const currentActive = document.querySelector(".page-item.active");
+
+  const currentIndex = Array.from(pageItems).indexOf(currentActive);
+
+  if (currentIndex < pageItems.length - 1) {
+    updateActivePage(currentIndex + 1);
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  const currentActive = document.querySelector(".page-item.active");
+
+  const currentIndex = Array.from(pageItems).indexOf(currentActive);
+
+  if (currentIndex > 0) {
+    updateActivePage(currentIndex - 1);
+  }
+});
